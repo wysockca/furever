7f8bcd6544fc0d819d3034accd17d9ffea4108ad
wysockca <wysockca@users.noreply.github.com>
2018-11-21 08:47:53 -0500

    Create front.php

---
+++ front.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Furever</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<header>
+			<a href=""><img src=""/></a>
+			<nav>
+				<ul>
+					<li>Browse</li>
+					<li>Profile</li>
+					<li>Login</li>
+					<li>Logout</li>
+					<li>Sign up</li>
+				</ul>
+				<div id="search">
+				</div>
+			</nav>
+		</header>
+		<main>
+			<section id="welcome">
+				
+			</section>
+			<section id="signup">
+				<form>
+					<input type="text" name="username" id="usernameInput"/>
+					<input type="email" name="email" id="emailInput" />
+					<input type="password" name="password" id="passwordInput" />
+					<input type="submit" value="Sign Up" id="submitBtn" />
+				</form>
+				<p>Are you an Animal Shelter? Click for the shelter sign up <a href="">here</a>.</p>
+			</section>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
